aca55d7b2039fe87bf2a27694cfb109f129fca1c
김영래 <rayray.yoyo@gmail.com>
2019-08-06 13:08:03 +0900

    [UPDATE] MainActivity

--- Android/app/src/main/java/com/example/android/MainActivity.kt
+++ Android/app/src/main/java/com/example/android/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import org.jetbrains.anko.sdk27.coroutines.onClick
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnSearch: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSearch = findViewById(R.id.fab_gosearch_main)
+        btnSearch.onClick {
+            startActivity<SearchActivity>()
+        }
     }
 }
